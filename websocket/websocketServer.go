f2c459c59586f92f83bdbc37d133c58980ab88e7
João Otávio Cano <joaootaviocano@gmail.com>
2024-04-15 01:25:30 -0300

    sevrer side request validation

--- websocket/websocketServer.go
+++ websocket/websocketServer.go
@@ -11,13 +11,24 @@ type WebSocketServer struct {}
 
 func (ws *WebSocketServer) handshake(w http.ResponseWriter, r *http.Request) {
 
+	checkReqMethod := r.Method == "GET"
+	checkUpgradeHeader := len(r.Header[http.CanonicalHeaderKey("Upgrade")]) == 1 && r.Header[http.CanonicalHeaderKey("Upgrade")][0] == "websocket"
+	checkConnectionHeader := len(r.Header[http.CanonicalHeaderKey("Connection")]) == 1 && r.Header[http.CanonicalHeaderKey("Connection")][0] == "Upgrade"
+	checkWebsocketVersionHeader := len(r.Header[http.CanonicalHeaderKey("Sec-WebSocket-Version")]) == 1 && r.Header[http.CanonicalHeaderKey("Sec-WebSocket-Version")][0] == "13"
+	checkWebsocketKeyHeader := len(r.Header[http.CanonicalHeaderKey("Sec-WebSocket-Key")]) == 1
+
+	if !checkReqMethod || !checkUpgradeHeader || !checkConnectionHeader || !checkWebsocketKeyHeader || !checkWebsocketVersionHeader {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	header := http.CanonicalHeaderKey("Sec-WebSocket-Key")
 	websocketKey := r.Header[header][0]
 
 	websocketAcceptValue := CreateWebsocketAcceptValue(websocketKey) 
 	
 	w.Header().Add("Sec-WebSocket-Accept", websocketAcceptValue)
-	w.Header().Add("Upgarde", "websocket")
+	w.Header().Add("Upgrade", "websocket")
 	w.Header().Add("Connection", "Upgrade")
 	w.WriteHeader(http.StatusSwitchingProtocols)
 	
