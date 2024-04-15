3c11b167d70246400e2ccc8d64b543b5af2bbe9a
João Otávio Cano <joaootaviocano@gmail.com>
2024-04-15 00:59:20 -0300

    Client side handshake response validation

--- websocket/websocketClient.go
+++ websocket/websocketClient.go
@@ -1,6 +1,8 @@
 package websocket
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -11,18 +13,24 @@ type WebSocketClient struct {
 }
 
 func (c *WebSocketClient) handshake(rawURL string) error {
-	
+
 	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	
+
+	websocketKey, err := createWebsocketKeyValue()
+	if err != nil {
+		return nil
+	}
+
 	h := &http.Header{}
 	h.Add("Host", parsedURL.Host)
 	h.Add("Upgrade", "websocket")
 	h.Add("Connection", "Upgrade")
-	h.Add("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
-	
+	h.Add("Sec-WebSocket-Key", websocketKey)
+	h.Add("Sec-WebSocket-Version", "13")
+
 	req := &http.Request{
 		Method: "GET",
 		Header: *h,
@@ -31,13 +39,36 @@ func (c *WebSocketClient) handshake(rawURL string) error {
 
 	resp, err := c.client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	fmt.Println(resp)
+
+	checkStatusCode := resp.StatusCode == http.StatusSwitchingProtocols
+	checkUpgradeHeader := len(resp.Header[http.CanonicalHeaderKey("Upgrade")]) == 1 && resp.Header[http.CanonicalHeaderKey("Upgrade")][0] == "websocket"
+	checkConnectionHeader := len(resp.Header[http.CanonicalHeaderKey("Connection")]) == 1 && resp.Header[http.CanonicalHeaderKey("Connection")][0] == "Upgrade"
+	checkWebsocketAcceptHeader := len(resp.Header[http.CanonicalHeaderKey("Sec-WebSocket-Accept")]) == 1 && resp.Header[http.CanonicalHeaderKey("Sec-WebSocket-Accept")][0] == CreateWebsocketAcceptValue(websocketKey)
+
+
+	if !checkStatusCode || !checkUpgradeHeader || !checkConnectionHeader || !checkWebsocketAcceptHeader {
+		fmt.Println("handshake error: connection not established")
+		return fmt.Errorf("handshake error: connection not established")
+	}
 
 	return nil
 }
 
 func (wc *WebSocketClient) Connect(url string) {
 	wc.handshake(url)
+}
+
+func createWebsocketKeyValue() (string, error) {
+	nonce := make([]byte, 16)
+
+	_, err := rand.Read(nonce)
+	if err != nil {
+		return "", err
+	}
+
+	nonceBase64 := base64.StdEncoding.EncodeToString(nonce)
+
+	return nonceBase64, nil
 }
